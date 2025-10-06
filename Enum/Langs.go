e648b47a8af9a0c62973db4860a627dd57b366c8
Nicolas-Asafe <nicolasasafe45@gmail.com>
2025-10-06 16:52:49 -0300

    fix: Langs.go of Enum package

--- Enum/Langs.go
+++ Enum/Langs.go
@@ -12,25 +12,26 @@ func (l Lang) String() string {
 	return [...]string{"JavaScript", "GoLang", "Java"}[l]
 }
 
-func (l Lang) MainCodeExample() string {
+// Retorna [nome do arquivo, conteúdo] para cada linguagem
+func (l Lang) MainCodeExample() []string {
 	switch l {
 	case JavaScript:
-		return `console.log("Hello!");`
+		return []string{"script.js", `console.log("Hello!");`}
 	case GoLang:
-		return `package main
+		return []string{"main.go", `package main
 
 import "fmt"
 
 func main() {
 	fmt.Println("Hello!")
-}`
+}`}
 	case Java:
-		return `public class Main {
+		return []string{"Main.java", `public class Main {
 	public static void main(String[] args) {
 		System.out.println("Hello!");
 	}
-}`
+}`}
 	default:
-		return ""
+		return []string{"", ""}
 	}
 }
