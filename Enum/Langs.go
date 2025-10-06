511305abde5242f2f7144a1da929fa0100887e6a
Nicolas-Asafe <nicolasasafe45@gmail.com>
2025-10-06 14:19:57 -0300

    feat: MainCodeExample in enum langs.go

--- Enum/Langs.go
+++ Enum/Langs.go
@@ -1,14 +1,36 @@
 package enum
 
-
 type Lang int
 
 const (
 	JavaScript Lang = iota
-	GoLang 
+	GoLang
 	Java
 )
 
 func (l Lang) String() string {
 	return [...]string{"JavaScript", "GoLang", "Java"}[l]
+}
+
+func (l Lang) MainCodeExample() string {
+	switch l {
+	case JavaScript:
+		return `console.log("Hello!");`
+	case GoLang:
+		return `package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("Hello!")
+}`
+	case Java:
+		return `public class Main {
+	public static void main(String[] args) {
+		System.out.println("Hello!");
+	}
+}`
+	default:
+		return ""
+	}
 }
