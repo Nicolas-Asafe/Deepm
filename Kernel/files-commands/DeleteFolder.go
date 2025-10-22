3385a7eba7db1bad9307a171b0f2c22bf3a1f231
Nicolas-Asafe <nicolasasafe45@gmail.com>
2025-10-22 17:58:52 -0300

    fix: delete folder (file command)

--- Kernel/files-commands/DeleteFolder.go
+++ Kernel/files-commands/DeleteFolder.go
@@ -5,11 +5,13 @@ import (
 	"os"
 )
 
-func DeleteFolder(path string) {
-	err := os.Remove(path)
+func DeleteFolder(path string) bool {
+	err := os.RemoveAll(path)
 	if err != nil {
 		fmt.Println("Error for delete folder:", err)
+		return false
 	} else {
 		fmt.Println("Folder deleted successfully")
 	}
+	return true
 }
